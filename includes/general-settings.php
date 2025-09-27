22a8ed2081bda6eab8e1aaa45f4f5d7c1716d662
DAnn2012 <dannunziata@hotmail.it>
2025-09-27 23:28:59 +0200

    Update general-settings.php

--- includes/general-settings.php
+++ includes/general-settings.php
@@ -248,7 +248,7 @@ if (false !== $status && $status == 'valid') {
 									}	
 
 									if ( ! $expired && ! empty ( $expires ) && $expires !== 'lifetime' ) {
-										esc_html_e( sprintf( 'Expires on %s', $expires ), 'pmpro-pdf-invoices' );
+										printf( esc_html__( 'Expires on %s', 'pmpro-pdf-invoices' ), $expires );
 									}
 								} else {
 									?>
